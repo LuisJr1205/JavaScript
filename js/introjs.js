52f4ab1e8fc2b826df5995fe6de44a27177588d1
Luis <lmadero@ari.group>
2023-02-27 16:45:26 -0600

    first commit

---
+++ js/introjs.js
@@ -0,0 +1,17 @@
+// Comentario
+/*Comentario multilinea*/
+
+// alert("Hola Mundo!");
+// alert("Hola Mundo2!");
+
+// Conosola en java script
+
+/*console.log('Hola saludamos a la console');
+console.log(2+2);
+console.info('Mostrando una informacion');
+console.warn('Estamos mostrando una advertencia');
+console.error('Mostrando un error');*/
+
+// Document write
+
+document.write('Este mensaje se mostrara en mi pagina web');
